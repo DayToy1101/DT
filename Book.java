8f5640eda2577dad385a6f52c849247811b2356d
DT <2573263498@qq.com>
2019-11-16 17:01:24 +0800

    20191116

---
+++ Book.java
@@ -0,0 +1,27 @@
+package book;
+
+public class Book {
+    public String name;//书名
+    public String author;//作者名
+    public int price;//价格
+    public String type;//类型
+    public boolean isBorrowed;//是否被借出;
+
+    public Book(String name, String author, int price, String type) {
+        this.name = name;
+        this.author = author;
+        this.price = price;
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "name='" + name + '\'' +
+                ", author='" + author + '\'' +
+                ", price=" + price +
+                ", type='" + type + '\'' +
+                ", isBorrowed=" + isBorrowed +
+                '}';
+    }
+}
