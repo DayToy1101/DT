1f95fad550988f3ba597f1c9982f974203d051f7
a <2573263498@qq.com>
2020-01-23 21:34:14 +0800

    20200123

---
+++ user/User.java
@@ -0,0 +1,15 @@
+package user;
+
+import book.BookList;
+import operation.IOperation;
+
+public abstract class User {
+    protected String name;
+    protected IOperation[] operations;
+
+    public abstract  int menu();
+
+    public void doOperation(int choice,BookList bookList){
+        operations[choice].work(bookList);
+    }
+}
